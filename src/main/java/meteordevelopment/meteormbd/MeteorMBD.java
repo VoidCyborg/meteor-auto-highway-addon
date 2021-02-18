fb58696e0bbe60164bb8febf48250518cee8b016
MineGame159 <petulko08@gmail.com>
2021-02-18 13:33:41 +0100

    Switched to new entrypoint addon system

--- src/main/java/meteordevelopment/meteormbd/MeteorMBD.java
+++ src/main/java/meteordevelopment/meteormbd/MeteorMBD.java
@@ -1,25 +1,18 @@
 package meteordevelopment.meteormbd;
 
-import meteordevelopment.orbit.EventHandler;
-import minegame159.meteorclient.MeteorClient;
-import minegame159.meteorclient.events.meteor.ClientInitialisedEvent;
+import minegame159.meteorclient.MeteorApi;
 import minegame159.meteorclient.modules.Modules;
 import minegame159.meteorclient.modules.misc.AutoMountBypassDupe;
-import net.fabricmc.api.ClientModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class MeteorMBD implements ClientModInitializer {
+public class MeteorMBD implements MeteorApi {
 	public static final Logger LOG = LogManager.getLogger();
 
 	@Override
-	public void onInitializeClient() {
+	public void onInitialize() {
 		LOG.info("Initializing Meteor MBD Addon");
-		MeteorClient.EVENT_BUS.subscribe(this);
-	}
 
-	@EventHandler
-	private void onMeteorInitialised(ClientInitialisedEvent event) {
 		Modules.get().addModule(new AutoMountBypassDupe());
-	};
+	}
 }
